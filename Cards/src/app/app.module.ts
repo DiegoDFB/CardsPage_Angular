9701baf8d4830970bec40b7a3c22c8cfd7010eb2
DiegoDFB <difrancisblanco@gmail.com>
2024-04-30 14:36:09 -0300

    Feito novos componentes de formulário

--- Cards/src/app/app.module.ts
+++ Cards/src/app/app.module.ts
@@ -1,22 +1,26 @@
-import {FormsModule} from '@angular/forms';
-import {MatRadioModule} from '@angular/material/radio';
-import {MatInputModule} from '@angular/material/input';
-import {MatSelectModule} from '@angular/material/select';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatIconModule } from '@angular/material/icon';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TarefaComponent } from './tarefa/tarefa.component';
 import { CardsComponent } from './cards/cards.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
 @NgModule({
   declarations: [
     AppComponent,
     TarefaComponent,
-    CardsComponent
+    CardsComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule,
@@ -25,7 +29,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatInputModule,
     MatRadioModule,
     MatSelectModule,
-    FormsModule
+    FormsModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    MatDatepickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
